16069f1cb966286bf07bf0791117d953040ea335
snew430 <seanpnew@hotmail.com>
2022-03-10 09:06:59 -0500

    initial setup

---
+++ controllers/home-routes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: [],
+    include: [
+      {
+        model: {},
+      },
+      {},
+    ],
+  })
+    .then((dbPostData) => {
+      console.log(dbPostData[0]);
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS digitech;
+CREATE DATABASE digitech;
